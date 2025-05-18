49b435c8a9bcb8f7a1512457ad173c5c82a2ba79
safaaAZ0421 <azbegsafae@gmail.com>
2025-05-18 23:04:37 +0100

    Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
